c1bf02d230c1e353e3b3b222f52b6bd12aae03ff
danielabreuoliveira <danieloliveirabsi@gmail.com>
2021-03-06 15:58:57 -0300

    primeiro commit no repositorio

---
+++ exercicios/fundamentos/parNomeValor.js
@@ -0,0 +1,20 @@
+const saudacao = 'Opa'
+
+function exec(){
+    const saudacao = 'falaaa'
+    return saudacao
+}
+
+const cliente ={
+    nome: 'Pedro',
+    idade: 32,
+    peso: 90,
+    endereco: {
+        logradouro: 'Rua mota',
+        numero: 123
+    }
+}
+
+console.log(saudacao )
+console.log(exec())
+console.log(cliente)
