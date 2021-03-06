f73676144c1c1c09f6742b3ba7079f8bdce5a104
danielabreuoliveira <danieloliveirabsi@gmail.com>
2021-03-06 18:21:15 -0300

    .

---
+++ exercicios/fundamentos/ternario.js
@@ -0,0 +1,9 @@
+const resultado = nota => nota >= 7 ? 'Aprovado' : 'Reprovado'
+
+console.log(resultado(7.1))
+console.log(resultado(6.3))
+
+const nota = 2
+const status = nota >= 7 ? 'Aprovado' : 'Reprovado'
+
+console.log(status)
