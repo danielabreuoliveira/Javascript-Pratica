ff17157a172e99aa72f6d7078eedf97d995fa0b6
03702072110 <daniel.oliveira@fnde.gov.br>
2021-03-09 14:16:02 -0300

    .

---
+++ exercicios/funcao/cidadao.js
@@ -0,0 +1,27 @@
+function func1() {
+
+}
+
+const func2 = function() {}
+
+const array = [function(a, b) { return a + b }, func1, func2]
+
+console.log(array[0](2, 3))
+
+const obj = {}
+obj.falar = function() { return 'Opa' }
+console.log(obj.falar())
+
+function run(fun) {
+    fun()
+}
+
+run(function() { console.log('Executando') })
+
+function soma(a, b) {
+    return function(c) {
+        console.log(a + b + c)
+    }
+}
+
+soma(4, 3)(3)
