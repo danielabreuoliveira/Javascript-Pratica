34f869d6a2d66d55d97d278f88e1666d8225fa5e
03702072110 <daniel.oliveira@fnde.gov.br>
2021-03-10 09:31:30 -0300

    .

--- exercicios/funcao/paramPadrao.js
+++ exercicios/funcao/paramPadrao.js
@@ -11,4 +11,16 @@ console.log(soma(), soma(3), soma(1, 2, 3), soma(0, 0, 0), soma(3, 0, 2))
 //gerar outro valor
 function soma1(a, b, c) {
     a = a !== undefined ? a : 1
+    b = 1 in arguments ? b : 1
+    c = isNaN(c) ? 1 : c
+    return a + b + c
 }
+
+console.log(soma1(), soma1(3), soma1(1, 2, 3), soma1(0, 0, 0))
+
+//padrao ES2015
+function soma2(a = 1, b = 1, c = 1) {
+    return a + b + c
+}
+
+console.log(soma2(), soma2(3), soma2(1, 2, 3), soma2(0, 0, 0))
