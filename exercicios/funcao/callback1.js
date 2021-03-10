dd74f9dbda4ccf9795e0e5c21d5df2e63a4ee7b6
03702072110 <daniel.oliveira@fnde.gov.br>
2021-03-10 14:26:07 -0300

    .

---
+++ exercicios/funcao/callback1.js
@@ -0,0 +1,19 @@
+const fabricantes = ["Mercedez", "Audi", "BMW"]
+
+function imprimir(nome, indice) {
+    console.log(`${indice + 1}. ${nome}`)
+}
+
+fabricantes.forEach(imprimir)
+    /*fabricantes.forEach(function(fabricante) {
+        console.log(fabricante)
+    })*/
+fabricantes.forEach(fabricante => console.log(fabricante))
+
+const times = ["Vasco", "Fluminense", "Flamengo", "botafogo"]
+
+function imprimirTime(nome, indice) {
+    console.log(`${inidice+1}. ${nome}`)
+}
+
+times.forEach(imprimir)
